57531a03afba8a5ea978d2133bc3da6f3fd0639a
chris.mohan <chris.mohan@captiveaire.com>
2019-04-26 15:09:53 -0400

    cleanup

--- src/CaptiveAire.Gotenberg.App.API.Client/Extensions/GotenbergSharpRequestExtensions.cs
+++ src/CaptiveAire.Gotenberg.App.API.Client/Extensions/GotenbergSharpRequestExtensions.cs
@@ -22,39 +22,7 @@ namespace CaptiveAire.Gotenberg.App.API.Sharp.Client.Extensions
         {
             var docParts = ToHttpContentCollection(request, request.Content.GetType());
             var dimParts = ToHttpContentCollection(request, request.Dimensions.GetType());
-
             docParts.AddRange(dimParts);
-
-            return docParts;
-
-            /*var docParts = request.Content
-                                  .GetType()
-                                  .GetProperties()
-                                  .Where(prop => Attribute.IsDefined(prop, typeof(MultiFormHeaderAttribute)))
-                                  .Select(p=> new { Prop = p , Attrib = (MultiFormHeaderAttribute)Attribute.GetCustomAttribute(p, typeof(MultiFormHeaderAttribute)) } )
-                                  .Select(_ =>
-                                             {
-                                                 var value = _.Prop.GetValue(request.Content);
-                                                 var docPart = new StringContent(value.ToString());
-                                                 docPart.Headers.ContentDisposition = new ContentDispositionHeaderValue(_.Attrib.ContentDisposition) { Name = _.Attrib.Name , FileName = _.Attrib.FileName };
-                                                 docPart.Headers.ContentType = new MediaTypeHeaderValue(_.Attrib.MediaType);
-
-                                                 return docPart;
-                                             }).ToList();
-            var docDims = request.Dimensions
-                                 .GetType()
-                                 .GetProperties()
-                                 .Where(prop => Attribute.IsDefined(prop, typeof(MultiFormHeaderAttribute)))
-                                 .Select(p=> new { Prop = p , Attrib = (MultiFormHeaderAttribute)Attribute.GetCustomAttribute(p, typeof(MultiFormHeaderAttribute)) } )
-                                 .Select(_ =>
-                                         {
-                                             var value = _.Prop.GetValue(request.Dimensions);
-                                             var dimContent = new StringContent(value.ToString());
-                                             dimContent.Headers.ContentDisposition = new ContentDispositionHeaderValue(_.Attrib.ContentDisposition) { Name = _.Attrib.Name };
-                                             return dimContent;
-                                    }).ToList();
-
-            docParts.AddRange(docDims);*/
 
             return docParts;
         }
