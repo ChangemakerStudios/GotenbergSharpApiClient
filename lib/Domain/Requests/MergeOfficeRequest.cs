58379dbf833d7633ccb694fc83dd8e2feb550cb0
Chris Mohan <chris.mohan@captiveaire.com>
2019-11-21 19:37:33 -0500

    Office merges now work (when Office LibreOffice is not enabled in your docker compose).  Request config works for HTML conversions but does not for office merges.  Calling ConfigureWith on a merge produces a null ref exception.

---
+++ lib/Domain/Requests/MergeOfficeRequest.cs
@@ -0,0 +1,9 @@
+// Gotenberg.Sharp.Api.Client - Copyright (c) 2019 CaptiveAire
+
+namespace Gotenberg.Sharp.API.Client.Domain.Requests
+{
+    public class MergeOfficeRequest
+    {
+        
+    }
+}
