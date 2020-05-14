cf4863dcdd576b4266ace5168815c59b899938f4
Jaben Cargman <jaben.cargman@captiveaire.com>
2020-05-14 18:13:05 -0400

    Minor -- added ResolveContentType abstraction and added the AddRange/IfNullEmpty() extensions.
    # Conflicts:
    #	lib/Domain/Requests/DocumentRequest.cs
    #	lib/Domain/Requests/Facets/AssetDictionary.cs
    #	lib/Domain/Requests/Facets/RequestConfig.cs
    #	lib/Domain/Requests/PdfRequest.cs
    #	lib/GotenbergSharpClient.cs

---
+++ lib/Domain/ContentTypes/IResolveContentType.cs
@@ -0,0 +1,9 @@
+﻿// Gotenberg.Sharp.Api.Client - Copyright (c) 2020 CaptiveAire
+
+namespace Gotenberg.Sharp.API.Client.Domain.ContentTypes
+{
+    public interface IResolveContentType
+    {
+        string GetContentType(string fileName, string defaultContentType = "application/octet-stream");
+    }
+}
